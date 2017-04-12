021f93fcfae7fdfca5f97f68c4bea05bc6b02c47
jonnyshaw89 <jon.shaw@jassoft.co.uk>
2017-04-12 15:48:54 +0100

    Restrict DB access

--- security_groups.tf
+++ security_groups.tf
@@ -61,6 +61,6 @@ resource "aws_security_group" "concourse_db_security_group" {
     from_port   = "5432"
     to_port     = "5432"
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${var.public_subnet_cidr}"]
   }
 }
