ee2f1ff96b75b79c0161671d707906e051403b27
jonnyshaw89 <jon.shaw@jassoft.co.uk>
2017-10-04 12:20:13 +0100

    Updated image

--- vars.tf
+++ vars.tf
@@ -24,7 +24,8 @@ variable "vpc_cidr" {
 variable "public_subnet_cidr" {
   type        = "list"
   description = "CIDR for the Public Subnet"
-  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  default     = ["10.0.1.0/24"]
+//  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 
 variable "private_subnet_cidr" {
